9c16aba25a8239eab8292fbadb779a4774213af6
Brom95 <warfard34@gmail.com>
2021-12-18 14:29:23 +0300

    VK sender work start

---
+++ CrossPost/MessageSender/VKPersonalPageMessageSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossPost.MessageSender
+{
+    internal class VKPersonalPageMessageSender : MessageSenderBase
+    {
+        public override Task Send(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
