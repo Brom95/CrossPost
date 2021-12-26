61d24257786a0e2a3cdd84e1c411f4ac0108610c
Brom95 <warfard34@gmail.com>
2021-12-26 13:20:35 +0300

    Plugins post build copy

---
+++ ConsoleSender/ConsoleSender.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Sender;
+
+namespace ConsoleSender;
+public class ConsoleSender : ISender
+{
+    public async Task Send(Message.Message message)
+    {
+        Console.WriteLine(message.Text);
+    }
+}
+
+public class ConsoleSenderFactory : ISenderFactory
+{
+    public ISender CreateSender(IConfigurationSection receiverSection)
+    {
+        return new ConsoleSender();
+    }
+}
